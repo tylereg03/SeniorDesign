e724bd8ecaf89fe74de2cb5b2f9b222502d2ad99
Niblick1020 <wpsebastian1020@gmail.com>
2024-01-25 16:05:09 -0800

    Made a lot of changes to assignment

--- Sebastian-Gomez_CollectFiles.py
+++ Sebastian-Gomez_CollectFiles.py
@@ -1,108 +1,62 @@
-import json
 import requests
+import json
 import csv
-
 import os
 
-if not os.path.exists("data"):
- os.makedirs("data")
+repo = 'scottyab/rootbeer'
 
-# GitHub Authentication function
-def github_auth(url, lsttoken, ct):
+def github_auth(url, lstTokens, ct):
     jsonData = None
     try:
         ct = ct % len(lstTokens)
-        headers = {'Authorization': 'Bearer {}'.format(lsttoken[ct])}
+        headers = {'Authorization': 'Bearer {}'.format(lstTokens[ct])}
         request = requests.get(url, headers=headers)
         jsonData = json.loads(request.content)
         ct += 1
     except Exception as e:
-        pass
         print(e)
     return jsonData, ct
 
-# @dictFiles, empty dictionary of files
-# @lstTokens, GitHub authentication tokens
-# @repo, GitHub repo
-def countfiles(dictfiles, lsttokens, repo):
-    ipage = 1  # url page counter
-    ct = 0  # token counter
+def get_source_files(repo, lstTokens):
+    files = []
+    ct = 0
+    url = f'https://api.github.com/repos/{repo}/git/trees/master?recursive=1'
+    jsonData, ct = github_auth(url, lstTokens, ct)
+    if jsonData:
+        for file in jsonData['tree']:
+            if file['type'] == 'blob' and file['path'].endswith(('.py', '.js', '.java', '.cpp', '.c')):
+                files.append(file['path'])
+    return files
 
-    try:
-        # loop though all the commit pages until the last returned empty page
-        while True:
-            spage = str(ipage)
-            commitsUrl = f'https://api.github.com/repos/{repo}/commits?page={spage}&per_page=100'
-            jsonCommits, ct = github_auth(commitsUrl, lsttokens, ct)
+def get_commit_history(file, repo, lstTokens):
+    commit_history = []
+    ct = 0
+    url = f'https://api.github.com/repos/{repo}/commits?path={file}'
+    jsonData, ct = github_auth(url, lstTokens, ct)
+    if jsonData:
+        for commit in jsonData:
+            author = commit['commit']['author']['name']
+            date = commit['commit']['author']['date']
+            commit_history.append((author, date))
+    return commit_history
 
-            # break out of the while loop if there are no more commits in the pages
-            if len(jsonCommits) == 0:
-                break
-            # iterate through the list of commits in  spage
-            for shaObject in jsonCommits:
-                sha = shaObject['sha']
-                commit_author = shaObject['commit']['author']['name']
-                commit_date = shaObject['commit']['author']['date']
+def main():
+    lstTokens = ["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"]  # Add your GitHub tokens here
+    fileOutput = 'data/file_' + repo.split('/')[1] + '.csv'
 
-                # For each commit, use the GitHub commit API to extract the files touched by the commit
-                shaUrl = f'https://api.github.com/repos/{repo}/commits/{sha}'
-                shaDetails, ct = github_auth(shaUrl, lsttokens, ct)
-                filesjson = shaDetails['files']
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
-                for filenameObj in filesjson:
-                    filename = filenameObj['filename']
+    source_files = get_source_files(repo, lstTokens)
 
-                    if 'src' not in filename:
-                        continue
+    with open(fileOutput, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['File Name', 'Author', 'Date'])
 
-                    if filename not in dictfiles:
-                        dictfiles[filename] = {'count': 0, 'commits': []}
-                    dictfiles[filename]['count'] += 1
-                    dictfiles[filename]['commits'].append({'author': commit_author, 'date': commit_date, 'filename': filename})
-            ipage += 1
-    except Exception as e:
-        print(f"Error receiving file data: {e}")
-        exit(0)
-
-
+        for file in source_files:
+            commit_history = get_commit_history(file, repo, lstTokens)
+            for author, date in commit_history:
+                writer.writerow([file, author, date])
 
 if __name__ == "__main__":
-    # GitHub repo
-    repo = 'scottyab/rootbeer'
-    # repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
-    # repo = 'k9mail/k-9' # This repo is commit heavy. It takes long to finish executing
-    # repo = 'mendhak/gpslogger'
-
-
-    # put your tokens here
-    # Remember to empty the list when going to commit to GitHub.
-    # Otherwise they will all be reverted and you will have to re-create them
-    # I would advise to create more than one token for repos with heavy commits
-    lstTokens = ["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"]
-
-    dictfiles = dict()
-    countfiles(dictfiles, lstTokens, repo)
-
-    for key, value in dictfiles.items():
-        print(f"{key}: {value}\n\n")
-
-    print('Total number of files: ' + str(len(dictfiles)))
-
-    file = repo.split('/')[1]
-    # change this to the path of your file
-    fileOutput = 'data/file_' + file + '.csv'
-    rows = ["Filename", "Touches"]
-    fileCSV = open(fileOutput, 'w')
-    writer = csv.writer(fileCSV)
-    writer.writerow(rows)
-
-    bigcount = None
-    bigfilename = None
-    for filename, count in dictfiles.items():
-        rows = [filename, count]
-        writer.writerow(rows)
-        if bigcount is None or count > bigcount:
-            bigcount = count
-            bigfilename = filename
-    fileCSV.close()
-    print('The file ' + bigfilename + ' has been touched ' + str(bigcount) + ' times.')
+    main()
