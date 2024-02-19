947a1d82807a8cdc866546ad4ed20dc543d111ff
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-02-19 06:14:42 -0800

    Add documentation for old code to understand it better

--- chatGPT/Uyen_lotterySch.c
+++ chatGPT/Uyen_lotterySch.c
@@ -35,22 +35,26 @@ extern int sys_srand(void);
 // helper struct to keep track of range for each proc
 struct range_proc
 {
-    int range_start;
-    int range_end;
-    struct proc *proc;
+    int range_start;   // Start of the ticket range
+    int range_end;     // End of the ticket range
+    struct proc *proc; // Pointer to the process
 };
 
+// Function: scheduler
+// --------------------
+// Implements a lottery scheduler where processes are assigned tickets,
+// and a winner is chosen randomly. Processes with more tickets have
+// higher chances of winning the lottery.
 void scheduler(void)
 {
     struct proc *p;
     struct cpu *c = mycpu();
     int total_tickets = 0;
-    int ticket_ranges[NPROC][2];
-    // ticket_ranges[NPROC][2] mean the range of tickets for each process
-    struct range_proc range_procs[NPROC];
+    int ticket_ranges[NPROC][2];          // Array to store ticket ranges for each process
+    struct range_proc range_procs[NPROC]; // Array to store processes with their ticket ranges
     int i;
-    int rand_num = 0;
-    struct proc *winnerProc = 0;
+    int rand_num = 0;            // Random number generated for lottery
+    struct proc *winnerProc = 0; // Pointer to the winning process
 
     c->proc = 0;
     sys_srand(); // Seed the random number generator
@@ -66,13 +70,16 @@ void scheduler(void)
         {
             if (p->state != UNUSED)
             {
-
-                // Assign ticket range
+                // Assign ticket range for each process
+                // Ticket range is determined by the cumulative sum of tickets
+                // Example:
+                //  Process A with 10 tickets, Process B with 20 tickets
+                //  Process A: ticket range [0, 9], Process B: ticket range [10, 29]
                 ticket_ranges[i][0] = total_tickets;
                 ticket_ranges[i][1] = total_tickets + p->tickets - 1;
                 total_tickets += p->tickets;
 
-                // Store process pointer with range
+                // Store process pointer with its ticket range
                 range_procs[i].range_start = ticket_ranges[i][0];
                 range_procs[i].range_end = ticket_ranges[i][1];
                 range_procs[i].proc = p;
@@ -82,110 +89,127 @@ void scheduler(void)
         // If no runnable processes, skip
         if (total_tickets != 0)
         {
-
-            // Pick a random number
-            // and we mod the number by total_tickets
-            // bc we want the number to be within the range of total_tickets
+            // Pick a random number within the range of total tickets
             rand_num = sys_srand() % total_tickets;
 
             // Find the winner
             for (i = 0; i < NPROC; i++)
             {
-                // if not good
+                // Skip processes that are not runnable
                 if (range_procs[i].proc == 0 || range_procs[i].proc->state != RUNNABLE)
                     continue;
 
-                // Check if winner
+                // Check if the random number falls within the range of this process
                 if (rand_num >= range_procs[i].range_start && rand_num <= range_procs[i].range_end)
                 {
-
-                    // print winning number and range to verify that it work
-                    // printf("Winning number and range: %d, %d-%d\n", rand_num, range_procs[i].range_start, range_procs[i].range_end);
                     winnerProc = range_procs[i].proc;
                     break;
                 }
             }
         }
 
-        // If no winner, skip otherwise switch to winner
+        // If no winner, skip, otherwise switch to winner
         if (winnerProc != 0)
         {
-            // printf("have winner\n");
-            //  Switch to chosen process
+            // Switch to chosen process
             acquire(&winnerProc->lock);
             if (winnerProc->state == RUNNABLE)
             {
-                // printf("runnable\n");
-                //   Update ticket ranges if process completed
-                //   if (winnerProc->state == UNUSED)
-                //{
-                //  printf("unused\n");
+                // Update ticket ranges if process completed
                 for (i = 0; i < NPROC; i++)
                 {
-                    // recheck again
                     if (proc[i].state != UNUSED)
                     {
-                        // printf("1\n");
-                        //  ticket_range[i][0] mean the lower bound of the ticket range
-                        //  and if lower bound is greater than the winner's upper bound
-                        //  then we need to subtract winner's ticket from the lower bound
+                        // Adjust ticket ranges based on the winner's range
+                        // -------------------------------------------------
+                        // This section adjusts the ticket ranges of other processes based on the winning process's range.
+                        // The adjustments ensure that the winning process's tickets are properly accounted for and that
+                        // ticket ranges remain valid for all processes.
+
+                        // Case 1: Winner's range falls completely below the current process's range
+                        // ----------------------------------------------------------------------
+                        // If the start of the winner's range is greater than the end of the current process's range,
+                        // it means that the winner's range is completely below the current process's range.
+                        // In this case, both the start and end of the current process's range are decreased by the
+                        // number of tickets held by the winning process, effectively shifting the range upward.
                         if (ticket_ranges[i][0] > ticket_ranges[winnerProc->pid][1])
                         {
-                            // printf("2\n");
                             ticket_ranges[i][0] -= winnerProc->tickets;
                             ticket_ranges[i][1] -= winnerProc->tickets;
                         }
-                        // ticket_range[i][1] mean the upper bound of the ticket range
-                        // and if upper bound is less than the winner's lower bound
-                        else if (ticket_ranges[i][1] < ticket_ranges[winnerProc->pid][0])
-                        {
-                            // printf("3\n");
-                            //  Do nothing
-                        }
-                        // if winner's ticket range is within the range of another process
-                        // then we need to remove the winner's ticket range
-                        // by setting the range to -1
+
+                        // Case 2: Winner's range falls completely above the current process's range
+                        // -----------------------------------------------------------------------
+                        // If the end of the winner's range is less than the start of the current process's range,
+                        // it means that the winner's range is completely above the current process's range.
+                        // In this case, no adjustment is needed as the winner's range does not affect the current process's range.
+
+                        // Case 3: Winner's range completely encompasses the current process's range
+                        // -----------------------------------------------------------------------
+                        // If the start of the winner's range is greater than or equal to the start of the current process's range,
+                        // and the end of the winner's range is less than or equal to the end of the current process's range,
+                        // it means that the winner's range completely encompasses the current process's range.
+                        // In this case, the current process's range is invalidated by setting both the start and end to -1.
                         else if (ticket_ranges[i][0] >= ticket_ranges[winnerProc->pid][0] && ticket_ranges[i][1] <= ticket_ranges[winnerProc->pid][1])
                         {
-                            // printf("4\n");
                             ticket_ranges[i][0] = -1;
                             ticket_ranges[i][1] = -1;
                         }
-                        // if winner's ticket range is partially within the range of another process
-                        // then we need to update the range
+
+                        // Case 4: Winner's range partially overlaps with the current process's range (below)
+                        // -------------------------------------------------------------------------------
+                        // If the start of the winner's range is less than the start of the current process's range,
+                        // and the end of the winner's range is greater than the end of the current process's range,
+                        // it means that the winner's range partially overlaps with the lower part of the current process's range.
+                        // In this case, only the end of the current process's range is adjusted downwards by the
+                        // number of tickets held by the winning process, effectively shortening the range.
                         else if (ticket_ranges[i][0] < ticket_ranges[winnerProc->pid][0] && ticket_ranges[i][1] > ticket_ranges[winnerProc->pid][1])
                         {
-                            // printf("5\n");
                             ticket_ranges[i][1] -= winnerProc->tickets;
                         }
+
+                        // Case 5: Winner's range partially overlaps with the current process's range (above)
+                        // -------------------------------------------------------------------------------
+                        // If the start of the winner's range is greater than the start of the current process's range,
+                        // and the end of the winner's range is less than the end of the current process's range,
+                        // it means that the winner's range partially overlaps with the upper part of the current process's range.
+                        // In this case, only the end of the current process's range is adjusted upwards to one less than
+                        // the start of the winner's range, effectively truncating the range.
                         else if (ticket_ranges[i][0] < ticket_ranges[winnerProc->pid][0])
                         {
-                            // printf("6\n");
                             ticket_ranges[i][1] = ticket_ranges[winnerProc->pid][0] - 1;
                         }
+
+                        // Case 6: Winner's range partially overlaps with the current process's range (below)
+                        // -------------------------------------------------------------------------------
+                        // If the end of the winner's range is greater than the end of the current process's range,
+                        // and the start of the winner's range is less than the start of the current process's range,
+                        // it means that the winner's range partially overlaps with the lower part of the current process's range.
+                        // In this case, only the start of the current process's range is adjusted upwards to one more than
+                        // the end of the winner's range, effectively truncating the range.
                         else if (ticket_ranges[i][1] > ticket_ranges[winnerProc->pid][1])
                         {
-                            // printf("7\n");
                             ticket_ranges[i][0] = ticket_ranges[winnerProc->pid][1] + 1;
                         }
-                        // just to catch weird case not work
+
+                        // Default Case: Unexpected scenario
+                        // ----------------------------------
+                        // This case handles unexpected scenarios where the winner's range overlaps with the current process's range
+                        // in a way not covered by the previous cases. In such cases, the current process's range is invalidated by
+                        // setting both the start and end to -1.
                         else
                         {
-                            // printf("8\n");
                             ticket_ranges[i][0] = -1;
                             ticket_ranges[i][1] = -1;
                         }
                     }
                 }
-                //}
 
                 // Switch to chosen process
                 winnerProc->state = RUNNING;
                 c->proc = winnerProc;
                 swtch(&c->context, &winnerProc->context);
                 c->proc = 0;
-                // print winner num
-                // printf("winner num: %d\n", rand_num);
             }
             release(&winnerProc->lock);
         }
