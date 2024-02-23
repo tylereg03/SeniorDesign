bd30c7a4a427205d97e94679680817bfa9d0078d
tylereg03 <tylerevanguthrie@gmail.com>
2024-02-22 16:27:21 -0800

    Added ChatGPT suggestions to TylerGuthrie_josephusProblem.py

--- chatGPT/TylerGuthrie_josephusProblem.py
+++ chatGPT/TylerGuthrie_josephusProblem.py
@@ -1,26 +1,14 @@
-def Josh(person, k, index):
-  if len(person) == 1:
-    print(person[0])
-    return
-   
-  index = ((index+k)%len(person))
-   
-  person.pop(index)
-   
-  Josh(person,k,index)
- 
+def josephus(n, k):
+    survivor = 0  # Index of the survivor
+    for i in range(2, n + 1):
+        survivor = (survivor + k) % i
+    return survivor + 1  # Adding 1 to convert index to person number
+
 # Driver Program to test above function
-n = 14 # specific n and k  values for original josephus problem
+n = 14  # specific n and k values for original josephus problem
 k = 2
-k-=1   # (k-1)th person will be killed
- 
-index = 0
- 
-person=[]
-for i in range(1,n+1):
-  person.append(i)
- 
-Josh(person,k,index)
- 
-# This code is contributed by
-# Gaurav Kandel
+
+# Calculate the survivor
+survivor_number = josephus(n, k)
+
+print("The survivor is person number:", survivor_number)
